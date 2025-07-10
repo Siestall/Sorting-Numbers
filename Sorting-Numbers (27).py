69e50320c3d5280ae8d12eebce5de1365c3f69b3
Andrey <andrey.nevedskiy@mail.ru>
2025-07-10 10:25:14 +0300

    Add files via upload

---
+++ Sorting-Numbers (27).py
@@ -0,0 +1,22 @@
+from random import *
+
+k = []
+l = []
+n = []
+
+m = [choice([x for x in range(100)]) for x in range(randint(10,50))]
+
+for i in range(len(m)):
+    if i%2==0:
+        k.append(m[i])
+    else:
+        l.append(m[i])
+
+k = sorted(k)
+l = sorted(l)[::-1]
+
+for a in range(len(l)):
+    n += [k[a]] + [l[a]]
+
+print(m)
+print(n[::-1])
